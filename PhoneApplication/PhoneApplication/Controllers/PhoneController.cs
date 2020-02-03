972a85d75b7f459ee77c8c1dc93b452f4a67f8bf
Dorukhan Uzun <200427534@student.georgianc.on.ca>
2020-02-03 18:23:13 -0500

    completed lab3

---
+++ PhoneApplication/PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone { PhoneID = "1", PhoneName = "P30 Pro", Manufacturer = "Huawei", MSRP = 1060.00, ScreenSize = "6", DateReleased = DateTime.Parse("01-08-2018") },
+                new Phone { PhoneID = "2", PhoneName = "Iphone 11", Manufacturer = "Apple", MSRP = 900, ScreenSize = "5.2", DateReleased = DateTime.Parse("06.02.2019") },
+                new Phone { PhoneID = "3", PhoneName = "S10", Manufacturer = "Samsung", MSRP = 1090.00, ScreenSize = "6.2", DateReleased = DateTime.Parse("10-06-2017") }
+            };
+
+            return View(phoneList);
+        }
+    }
+}
---
+++ PhoneApplication/PhoneApplication/Models/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneID = "";
+            PhoneName = "";
+            Manufacturer = "";
+            MSRP = 0;
+            ScreenSize = "";
+            DateReleased = DateTime.Now;
+        }
+
+        public string PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public double MSRP { get; set; }
+        public string ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+
+
+
+
+
+}
